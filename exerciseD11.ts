a26fb9ac7531abf53376fb71067c054fac05b60c
crayoninvok <dathariqf@gmail.com>
2024-10-15 16:03:50 +0700

    Latihan tambahan FIbonacci

--- exerciseD11.ts
+++ exerciseD11.ts
@@ -159,3 +159,18 @@
 // console.log(ledderPlus(nums3));
 
 
+// 4. Fibonacci
+
+// function fib(n: number) {
+//     const res: number[] = [0,1]
+//     for (let i= 0; i< n; i++){
+//         res.push(res[i] + res[i+1])
+//     }
+
+//     return res[n+1]
+
+// }
+
+// console.log(fib(4));
+
+
