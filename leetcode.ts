b6956735924de6f7b4ac13f5b79480b74d477589
crayoninvok <dathariqf@gmail.com>
2024-10-15 15:58:49 +0700

    Latian

---
+++ leetcode.ts
@@ -0,0 +1,35 @@
+// function singleNumber(nums:number[]):number{
+//    let res = 0
+//     for (const num of nums) {
+//         res ^= num
+//     }
+
+//     return res
+// }
+
+// const nums1: number[] = [2,2,1]
+// const nums2:number[] = [4,1,2,1,2]
+// const nums3: number[] = [1]
+
+// const result1 = (singleNumber(nums1))
+// const result2 = (singleNumber(nums2))
+// const result3 = (singleNumber(nums3))
+
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+
+// function removeDuplicates(nums: number[]): number {
+//     const res:number[] = [...new Set(nums)]
+
+//     return res.length
+// }
+
+// const arr1 = [1,1,2]
+// const arr2 = [0,0,1,1,1,2,2,3,3,4,4]
+
+// console.log(removeDuplicates(arr1));
+// console.log(removeDuplicates(arr2));
+
+
